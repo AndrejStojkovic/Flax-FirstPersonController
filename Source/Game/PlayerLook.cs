859194e9af00f827b5d7a13110a2fdc60c8719f5
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2023-09-17 15:27:58 +0200

    Initial First Person Controller commit

---
+++ Source/Game/PlayerLook.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game
+{
+    public class PlayerLook : Script
+    {
+        public Actor Player;
+        public Actor CameraTarget;
+        public Camera Camera;
+        public Vector2 Sensitivity;
+        public float ClampX;
+        public float CameraSmoothing = 20f;
+
+        public bool Inverted = false;
+
+        private Vector2 rotation;
+
+        public override void OnStart()
+        {
+            rotation = new Vector2(Actor.Orientation.EulerAngles.X, Actor.Orientation.EulerAngles.Y);
+        }
+
+        public override void OnUpdate()
+        {
+            Screen.CursorVisible = false;
+            Screen.CursorLock = CursorLockMode.Locked;
+
+            var x = Input.GetAxisRaw("Mouse X") * Sensitivity.X;
+            var y = Input.GetAxisRaw("Mouse Y") * Sensitivity.Y;
+
+            var mouseDelta = new Float2(x, y);
+            if(Inverted) {
+                mouseDelta.Y *= -1;
+            }
+            rotation.X = Mathf.Clamp(rotation.X + mouseDelta.Y, -ClampX, ClampX);
+            rotation.Y += mouseDelta.X;
+        }
+
+        
+        public override void OnFixedUpdate() {
+            var camera = Camera.Transform;
+            var cameraFactor = Mathf.Saturate(CameraSmoothing * Time.DeltaTime);
+            CameraTarget.LocalOrientation = Quaternion.Lerp(CameraTarget.LocalOrientation, Quaternion.Euler(rotation.X, 0f, 0f), cameraFactor);
+            camera.Translation = Vector3.Lerp(camera.Translation, CameraTarget.Position, cameraFactor);
+            camera.Orientation = CameraTarget.Orientation;
+            Player.LocalOrientation = Quaternion.Euler(0f, rotation.Y, 0f);
+            Camera.Transform = camera;
+
+            // camera.Orientation = Quaternion.Lerp(camera.Orientation, Quaternion.Euler(rotation.X, rotation.Y, 0f), cameraFactor);
+            // Camera.Transform = camera;
+        }
+    }
+}
---
+++ Source/Game/PlayerMovement.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game {
+    public class PlayerMovement : Script {
+        public Actor Player;
+        public CharacterController PlayerController;
+        public Camera Camera;
+
+        [Header("Controller")]
+        public Vector3 ColliderStanding;
+        public float StandingHeight;
+        public float CameraOffsetY = 0f;
+
+        [Header("Movement")]
+        public float WalkSpeed;
+        public float RunSpeed;
+        public float MoveSmoothTime;
+
+        [Header("Crouch")]
+        public float CrouchHeight;
+        public float CrouchSpeed;
+        public bool CrouchToggle = false;
+        public Vector3 ColliderCrouching;
+        public bool isCrouching;
+
+        [Header("Jump")]
+        public float JumpStrength;
+        public float Gravity;
+
+        private Vector3 velocity;
+        private Vector3 currentForceVelocity;
+        private Vector3 moveDampVelocity;
+
+        public override void OnStart()
+        {
+            if(PlayerController == null) {
+                PlayerController = Actor.GetScript<CharacterController>();
+            }
+
+            SetCameraHeight();
+        }
+        
+        public override void OnFixedUpdate()
+        {
+            HandleCrouch();
+            HandleMovement();
+        }
+
+        protected void HandleMovement()
+        {
+            Vector3 PlayerInput = new Vector3 {
+                X = Input.GetAxisRaw("Horizontal"),
+                Y = 0f,
+                Z = Input.GetAxisRaw("Vertical")
+            };
+
+            PlayerInput.Normalize();
+
+            
+            Vector3 Move = Actor.Transform.TransformDirection(PlayerInput);
+            float CurrentSpeed = 0f;
+            
+
+            if(isCrouching) {
+                PlayerController.Center = ColliderCrouching;
+                PlayerController.Height = CrouchHeight;
+                CurrentSpeed = CrouchSpeed;
+            } else {
+                PlayerController.Center = ColliderStanding;
+                PlayerController.Height = StandingHeight;
+                CurrentSpeed = Input.GetKey(KeyboardKeys.Shift) ? RunSpeed : WalkSpeed;
+            }
+
+            SetCameraHeight();
+
+            CurrentSpeed *= 100;
+
+            velocity = Utils.SmoothDamp(
+                velocity,
+                Move * CurrentSpeed,
+                ref moveDampVelocity,
+                MoveSmoothTime
+            );
+
+            PlayerController.Move(velocity * Time.DeltaTime);
+
+            // TO-DO: Implement Gravity and Jumping
+            // PlayerController.Move(currentForceVelocity * Time.DeltaTime);
+        }
+
+        protected void HandleCrouch()
+        {
+            if(CrouchToggle) {
+                if(Input.GetKeyDown(KeyboardKeys.Control)) {
+                    isCrouching = !isCrouching;
+                }
+            } else {
+                if(Input.GetKeyDown(KeyboardKeys.Control)) {
+                    isCrouching = true;
+                } else if(Input.GetKeyUp(KeyboardKeys.Control)) {
+                    isCrouching = false;
+                }
+            }
+        }
+
+        protected void SetCameraHeight()
+        {
+            float height = isCrouching ? CrouchHeight : StandingHeight;
+            Camera.LocalPosition = new Vector3(0f, height + PlayerController.Radius + CameraOffsetY, 0f);
+        }
+    }
+}
